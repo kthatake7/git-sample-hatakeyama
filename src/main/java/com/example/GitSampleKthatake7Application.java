a5e18993aba4b65dd24945a475b9b01173b6f9ac
kthatake7 <kota.hatakeyama@rakus-partners.co.jp>
2020-02-04 12:08:19 +0900

    Carクラス追加

---
+++ src/main/java/com/example/GitSampleKthatake7Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleKthatake7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleKthatake7Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GitSampleKthatake7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitSampleKthatake7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
